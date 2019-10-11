a4e5964d581cc023af7dd0cfb1164310c5ca232c
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-10-11 09:37:58 -0700

    Style cards.

--- starwars/src/components/characterCard.js
+++ starwars/src/components/characterCard.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import styled from "styled-components";
+
+const Card = styled.div`
+  background: black;
+  color: white;
+width: 30%;
+opacity: .3;
+padding: 2%;
+margin: 2%;
+
+`;
+
 
 
 const CharacterCard = props => {
     return (
       
-          <div key={props.key}>
+          <Card >
             <h1>{props.name}</h1>
-            <h3>{props.birth_year}</h3>
-            <h3>{props.height}</h3>
-            <h3>{props.mass}</h3>
-            <h3>{props.skin_color}</h3>
-          </div>
+            <h3>Birth Year: {props.birth_year}</h3>
+            <h3>Hieght: {props.height}</h3>
+            <h3>Mass: {props.mass}</h3>
+            <h3>Skin tone: {props.skin_color}</h3>
+          </Card>
       
     );
   };
